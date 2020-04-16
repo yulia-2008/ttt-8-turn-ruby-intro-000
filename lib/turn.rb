c8aa41f5e6e3dd5058ec8e9252c9d7b3f7cb5eb6
Yuliya Yurkevich <juliana.ny2008@gmail.com>
2020-04-16 17:05:25 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -37,7 +37,7 @@ def turn(board)
 user_input=gets.strip 
 index= input_to_index(user_input)
  if valid_move?(board, index)
-move(board, index)
+# move(board, index)
 display(board, index)
 else turn(board)
 end
